cedd6eb47da0bd158574c5e2499b70baf590d75b
ViLan026 <vil782991@gmail.com>
2026-04-12 18:36:31 +0700

    fix comment

--- src/main/java/com/example/history_social_backend/modules/post/dto/request/PostSourceRequest.java
+++ src/main/java/com/example/history_social_backend/modules/post/dto/request/PostSourceRequest.java
@@ -1,8 +1,6 @@
 package com.example.history_social_backend.modules.post.dto.request;
 
 
-import java.util.UUID;
-
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
